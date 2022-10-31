ab4bc5da6652f43bc308aac63ac0c1764d4f120c
Travis <pandasquad.tb@gmail.com>
2022-10-31 12:57:55 -0700

    bugfix for exporting db connection

--- api/database/connection.js
+++ api/database/connection.js
@@ -2,4 +2,4 @@ const pgp = require('pg-promise')(/* options */)
 // Change to username:password for user with permissions to host:port/database
 const db = pgp('postgres://pandasquad:pandapassword@localhost:5432/user_search_test')
 
-exports.db = db
+module.exports = db;
