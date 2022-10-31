21abf13d92031b4aaa9f0026d23d14bbc777680e
Travis <pandasquad.tb@gmail.com>
2022-10-31 11:40:27 -0700

    clean up and fix index and users endpoints

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+const db = require('../database/connection')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/users', function(req, res, next) {
+  // Get all the users in the database
+  db.any('SELECT username FROM user_loc_test')
+  .then((data) => {
+    res.send(data);
+  })
+  .catch((error) => {
+    res.send(error);
+  })
 });
 
 module.exports = router;
