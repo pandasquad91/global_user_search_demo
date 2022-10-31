3fa2e9757271486da25d9651ecc034bffd62d9e0
Travis <pandasquad.tb@gmail.com>
2022-10-31 14:00:12 -0700

    bugfix, lon lat was swapped for sample data insertions

--- insert_sample_data.sql
+++ insert_sample_data.sql
@@ -1,13 +1,13 @@
 /* Sample data for testing */
 \c user_search_test
 
-INSERT INTO user_loc_test (username, geom) VALUES ('Seattle', ST_GeomFromText('POINT(47.37 -122.2)', 4326));
-INSERT INTO user_loc_test (username, geom) VALUES ('San Francisco', ST_GeomFromText('POINT(37.47 -122.25)', 4326));
-INSERT INTO user_loc_test (username, geom) VALUES ('Vancouver', ST_GeomFromText('POINT(49.15 -123.06)', 4326));
-INSERT INTO user_loc_test (username, geom) VALUES ('New York', ST_GeomFromText('POINT(40.40 -73.56)', 4326));
-INSERT INTO user_loc_test (username, geom) VALUES ('Toronto', ST_GeomFromText('POINT(43.42 -79.24)', 4326));
-INSERT INTO user_loc_test (username, geom) VALUES ('Orlando', ST_GeomFromText('POINT(28.25 -81.18)', 4326));
-INSERT INTO user_loc_test (username, geom) VALUES ('Sydney', ST_GeomFromText('POINT(-33.52 151.13)', 4326));
-INSERT INTO user_loc_test (username, geom) VALUES ('London', ST_GeomFromText('POINT(51.30 -0.08)', 4326));
-INSERT INTO user_loc_test (username, geom) VALUES ('Beijing', ST_GeomFromText('POINT(39.55 116.23)', 4326));
-INSERT INTO user_loc_test (username, geom) VALUES ('Cairo', ST_GeomFromText('POINT(30.03 31.14)', 4326));
+INSERT INTO user_loc_test (username, geom) VALUES ('Seattle', ST_GeomFromText('POINT(-122.2 47.37)', 4326));
+INSERT INTO user_loc_test (username, geom) VALUES ('San Francisco', ST_GeomFromText('POINT(-122.25 37.47)', 4326));
+INSERT INTO user_loc_test (username, geom) VALUES ('Vancouver', ST_GeomFromText('POINT(-123.06 49.15)', 4326));
+INSERT INTO user_loc_test (username, geom) VALUES ('New York', ST_GeomFromText('POINT(-73.56 40.40)', 4326));
+INSERT INTO user_loc_test (username, geom) VALUES ('Toronto', ST_GeomFromText('POINT(-79.24 43.42)', 4326));
+INSERT INTO user_loc_test (username, geom) VALUES ('Orlando', ST_GeomFromText('POINT(-81.18 28.25)', 4326));
+INSERT INTO user_loc_test (username, geom) VALUES ('Sydney', ST_GeomFromText('POINT(151.13 -33.52)', 4326));
+INSERT INTO user_loc_test (username, geom) VALUES ('London', ST_GeomFromText('POINT(-0.08 51.3)', 4326));
+INSERT INTO user_loc_test (username, geom) VALUES ('Beijing', ST_GeomFromText('POINT(116.23 39.55)', 4326));
+INSERT INTO user_loc_test (username, geom) VALUES ('Cairo', ST_GeomFromText('POINT(31.14 30.03)', 4326));
