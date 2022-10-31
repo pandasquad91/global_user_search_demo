2f278e73efdc95ce56caa725441e7024be0b445f
Travis <pandasquad.tb@gmail.com>
2022-10-30 21:05:36 -0700

    hooked express api to database

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,8 +1,19 @@
 var express = require('express');
 var router = express.Router();
+const pgp = require('pg-promise')(/* options */)
+const db = pgp('postgres://pandasquad:pandapassword@localhost:5432/user_search_test')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  console.log("Hello World!");
+  // Example database query
+  db.any('SELECT username FROM user_loc_test')
+  .then((data) => {
+    console.log('DATA:', data)
+  })
+  .catch((error) => {
+    console.log('ERROR:', error)
+  })
   res.render('index', { title: 'Express' });
 });
 
