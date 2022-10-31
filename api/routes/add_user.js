a306c84ee8e9c5540234a6aa95d7d3549011c32e
Travis <pandasquad.tb@gmail.com>
2022-10-31 15:23:20 -0700

    bugfixes for the insertion command to add a new user

--- api/routes/add_user.js
+++ api/routes/add_user.js
@@ -25,19 +25,24 @@ function valid(body) {
     return false;
 }
 
+/* Given a valid request body, generate the insertion command. */
+function generateInsertFromParams(params) {
+    let username = params.username;
+    let lon = params.lon;
+    let lat = params.lat;
+    return `INSERT INTO user_loc_test (username, geom) VALUES ('${username}', ST_GeomFromText('POINT(${lon} ${lat})', 4326))`;
+}
+
 /* Add a user. */
 router.post('/', function(req, res, next) {
     // validate request body has necessary and accurate parameters
-    if (valid(req.body)) {
-        // use elements in request body for insertion
-        db.none("INSERT INTO user_loc_test (username, geom) VALUES ('$username', ST_GeomFromText('POINT($lon $lat)', 4326))",
-        {
-            username: req.body.username,
-            lon: req.body.lon,
-            lat: req.body.lat
-        })
-        .then((data) => {
-            res.send(data);
+    if (valid(req.query)) {
+        // use elements in request body to generate the insertion command
+        let insert = generateInsertFromParams(req.query);
+        // insert the new user into the database
+        db.none(insert)
+        .then(() => {
+            res.send("Successfully inserted data");
         })
         .catch((error) => {
             res.send(error)
