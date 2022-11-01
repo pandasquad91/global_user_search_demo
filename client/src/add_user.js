ff784f592e9ce136c514c718faef6489d267043b
Travis <pandasquad.tb@gmail.com>
2022-11-01 13:13:38 -0700

    corrected api usage in add user

--- client/src/add_user.js
+++ client/src/add_user.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import addAPI from './api'
+import api from './api'
 
 
 class AddUser extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { lon: '', lat: '', username: '' };
+        this.state = { user_lon: '', user_lat: '', username: '', statusMsg: '' };
       }
   
       handleChange(event) {
@@ -13,19 +13,37 @@ class AddUser extends React.Component {
       }
 
       insertUser(event) {
-        fetch(addAPI, {lon: this.state.lon, lat: this.state.lat, username: this.state.username})
-        .then(res => console.log(`Successfully inserted user ${this.state.username}`));
+        function validateInput(input) {
+            // TODO: validate all the input
+            return true;
+        }
+
+        if (validateInput(this.state)) {
+            let lon = this.state.user_lon;
+            let lat = this.state.user_lat;
+            let username = this.state.username;
+
+            let requestOptions = {
+                method: 'POST',
+            }
+
+            fetch(api + `add?username=${username}&lon=${lon}&lat=${lat}`, requestOptions)
+            .then(res => this.state.statusMsg = `Successfully inserted new user ${this.state.username}`);
+        } else {
+            this.state.statusMsg = 'Could not insert user, check input data';
+        }
       }
   
       render() {
         return (
           <div>
             <form onSubmit={this.insertUser}>
-                <p>lon: </p> <input id="user_lat" type="text" value={this.state.lat} onChange={this.handleChange}></input>
-                <p>lat: </p> <input id="user_lon" type="text" value={this.state.lon} onChange={this.handleChange}></input>
+                <p>lon: </p> <input id="user_lat" type="text" value={this.state.user_lat} onChange={this.handleChange}></input>
+                <p>lat: </p> <input id="user_lon" type="text" value={this.state.user_lon} onChange={this.handleChange}></input>
                 <p>username: </p> <input id="username" type="text" value={this.state.username} onChange={this.handleChange}></input>
-                <input type="submit" value="Submit"></input>
+                <input type="submit" value="Add"></input>
             </form>
+            <p>{this.state.statusMsg}</p>
           </div>
           
         );
